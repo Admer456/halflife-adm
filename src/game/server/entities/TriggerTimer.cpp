b5542b6464a49f0cf801830534a762a72e169cf5
Admer <49600504+Admer456@users.noreply.github.com>
2022-01-13 22:16:04 +0100

    Implement TriggerTimer

---
+++ src/game/server/entities/TriggerTimer.cpp
@@ -0,0 +1,97 @@
+
+#include "extdll.h"
+#include "util.h"
+#include "cbase.h"
+
+#include "TriggerTimer.h"
+
+LINK_ENTITY_TO_CLASS(trigger_timer, TriggerTimer);
+
+TYPEDESCRIPTION TriggerTimer::m_SaveData[] =
+{	
+	DEFINE_FIELD(TriggerTimer, m_Activator, FIELD_CLASSPTR),
+	DEFINE_FIELD(TriggerTimer, m_Enabled, FIELD_BOOLEAN),
+	DEFINE_FIELD(TriggerTimer, m_UseType, FIELD_INTEGER),
+	DEFINE_FIELD(TriggerTimer, m_BaseDelay, FIELD_FLOAT),
+	DEFINE_FIELD(TriggerTimer, m_RandomDelay, FIELD_FLOAT)
+};
+
+IMPLEMENT_SAVERESTORE(TriggerTimer, CPointEntity);
+
+// ================================
+// TriggerTimer::Spawn
+// ================================
+void TriggerTimer::Spawn()
+{
+	CPointEntity::Spawn();
+
+	if (pev->spawnflags & SF_StartOn)
+	{
+		Use(this, this, USE_ON, 0.0f);
+	}
+}
+
+// ================================
+// TriggerTimer::KeyValue
+// ================================
+bool TriggerTimer::KeyValue(KeyValueData* pkvd)
+{
+	// Small experiment here to see if we can compare these more easier, without FStrEq
+	using namespace std::string_view_literals;
+	
+	if (pkvd->szKeyName == "baseDelay"sv)
+	{
+		m_BaseDelay = atof(pkvd->szValue);
+		return true;
+	}
+	else if (pkvd->szKeyName == "randomDelay"sv)
+	{
+		m_RandomDelay = atof(pkvd->szValue);
+		return true;
+	}
+
+	return CPointEntity::KeyValue(pkvd);
+}
+
+// ================================
+// TriggerTimer::Use
+// ================================
+void TriggerTimer::Use(CBaseEntity* activator, CBaseEntity* caller, USE_TYPE useType, float value)
+{
+	m_Enabled = !m_Enabled;
+	m_Activator = activator;
+	m_UseType = useType;
+
+	if (m_Enabled)
+	{
+		Tick();
+	}
+}
+
+// ================================
+// TriggerTimer::Think
+// ================================
+void TriggerTimer::Think()
+{
+	if (!m_Enabled)
+	{
+		return;
+	}
+
+	Tick(true);
+}
+
+// ================================
+// TriggerTimer::Tick
+// 
+// Triggers targets and thinks
+// ================================
+void TriggerTimer::Tick(bool useTargets)
+{
+	if (useTargets)
+	{
+		SUB_UseTargets(m_Activator, m_UseType, 0);
+	}
+
+	pev->nextthink = gpGlobals->time + m_BaseDelay + RANDOM_FLOAT(-m_RandomDelay, m_RandomDelay);
+}
---
+++ src/game/server/entities/TriggerTimer.h
@@ -0,0 +1,41 @@
+
+#pragma once
+
+// ================================
+// TriggerTimer
+//
+// A simple trigger timer with optional random timing
+// Formula: baseDelay + randomDelay * random(-1.0, 1.0)
+// ================================
+class TriggerTimer final : public CPointEntity
+{ // would probably be more logical to inherit from trigger_relay, but alas
+public:
+
+	static constexpr uint32_t SF_StartOn = 1 << 0;
+
+	void			Spawn() override;
+	bool			KeyValue(KeyValueData* pkvd) override;
+	
+	void			Use(CBaseEntity* activator, CBaseEntity* caller, USE_TYPE useType, float value) override;
+	void			Think() override;
+
+	bool			Save(CSave& save) override;
+	bool			Restore(CRestore& restore) override;
+
+	static TYPEDESCRIPTION m_SaveData[];
+
+private:
+	void			Tick(bool useTargets = false);
+
+private:
+	// When this entity gets triggered by another, remember the
+	// original activator to maintain the trigger chain
+	CBaseEntity*	m_Activator{nullptr};
+	USE_TYPE		m_UseType{USE_OFF};
+	// State management
+	bool			m_Enabled{false};
+
+	// Properties set by the mapper
+	float			m_BaseDelay{0.0f};
+	float			m_RandomDelay{0.0f};
+};
